adfe9b33df3847bfe59fde845f8164fc44b558e4
miguelsuafa <miguelsuafa@gmail.com>
2024-04-04 21:18:36 -0500

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ const router = Router()
 //Enrutamiento
 router.get('/', (req, res) => res.render('index', { title: 'Bienvenido - Home'}))
 router.get('/about', (req, res) => res.render('about', { title: 'About'}))
+router.get('/skills', (req, res) => res.render('skills', { title: 'skills'}))
+router.get('/projects', (req, res) => res.render('projects', { title: 'projects'}))
 router.get('/contact', (req, res) => res.render('contact', { title: 'Contact'}))
 
 export default router 
